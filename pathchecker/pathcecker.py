30f9592a024768833c57fc32364d1fc91b1cee4c
jaguschj <45715268+jaguschj@users.noreply.github.com>
2020-11-21 21:40:55 +0100

    define as function

--- pathchecker/pathcecker.py
+++ pathchecker/pathcecker.py
@@ -5,11 +5,13 @@ Created on Sun Nov 15 18:56:54 2020
 @author: JensJ
 """
 import os
-path=os.getenv('path')
-print(path)
-for p in path.split(';'):
-    if os.path.exists(p):
-        print('OK',p)
-    else:
-        print('Error',p)
-        
+def checkpath():
+    path=os.getenv('path')
+    print(path)
+    for p in path.split(';'):
+        if os.path.exists(p):
+            print('OK',p)
+        else:
+            print('Error',p)
+if __name__=='__main__':
+    checkpath()        
